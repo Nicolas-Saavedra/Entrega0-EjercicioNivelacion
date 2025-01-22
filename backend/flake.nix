7cb31bb4b5b86f51e034a35be1ba9b94c9df7984
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-01-21 20:10:22 -0500

    feat: initial commit

---
+++ backend/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A flake to set up Python 3.12 in a project environment";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShells.x86_64-linux.default =
+        let
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python312 # Python 3.12
+            python312Packages.pip # pip
+            cargo
+            rustc
+          ];
+
+          # Optional: Define any shell hooks or additional environment variables
+          shellHook = ''
+            exec fish
+          '';
+        };
+    };
+}
