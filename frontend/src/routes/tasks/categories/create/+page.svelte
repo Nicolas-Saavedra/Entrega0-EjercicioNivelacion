1800085feb918f29bf3436e65088b72bf62bb17c
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-01-27 19:56:08 -0500

    feat: add create category, main functionality done

--- frontend/src/routes/tasks/categories/create/+page.svelte
+++ frontend/src/routes/tasks/categories/create/+page.svelte
@@ -2,23 +2,20 @@
 	import * as Card from '$lib/components/ui/card';
 	import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
-	import PresetCalendar from '$lib/components/ui/preset-calendar/presetCalendar.svelte';
-	import PresetHour from '$lib/components/ui/preset-hour/presetHour.svelte';
 	import { Button } from '$lib/components/ui/button';
-	import * as Select from '$lib/components/ui/select';
 	import api from '$lib/api';
 	import { PUBLIC_API_URL } from '$env/static/public';
-	import { userTasks } from '../../../../stores/userTasks';
 	import { goto } from '$app/navigation';
-	import { taskCategories } from '../../../../stores/taskCategories';
 	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
+	import type { Category } from '../../../../types/category';
+	import { taskCategories } from '../../../../stores/taskCategories';
 
 	let categoryName: string | null = $state(null);
 	let categoryDescription: string | null = $state(null);
 
 	async function createCategory() {
 		if (categoryName && categoryDescription) {
-			const createTaskResponse = await api.post(
+			const createCategoryResponse = await api.post<Category>(
 				`${PUBLIC_API_URL}/categorias`,
 				{
 					nombre: categoryName,
@@ -28,8 +25,10 @@
 					validateStatus: (status) => status >= 200 && status <= 300
 				}
 			);
-			userTasks.update((current) => (current ? [...current, createTaskResponse.data] : null));
-			goto('/tasks');
+			taskCategories.update((current) =>
+				current ? [...current, createCategoryResponse.data] : null
+			);
+			goto('/tasks/categories');
 		}
 	}
 </script>
@@ -47,9 +46,9 @@
 			</div>
 			<div class="grid gap-3">
 				<Label for="name">Descripcion de la categoria</Label>
-				<Textarea id="description" class="w-full" bind:value={categoryName} />
+				<Textarea id="description" class="w-full" bind:value={categoryDescription} />
 			</div>
-			<Button class="mt-2 w-48">Crear nueva categoria</Button>
+			<Button class="mt-2 w-48" onclick={createCategory}>Crear nueva categoria</Button>
 		</div>
 	</Card.Content>
 </Card.Root>
