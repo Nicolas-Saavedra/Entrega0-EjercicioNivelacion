8153c06947c7265b819bd247c22823cca600a487
Nicolas-Saavedra <personal@nicolassaavedra.com>
2025-01-27 19:49:03 -0500

    feat: add refresh token to interceptor prpoperly

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -26,4 +26,47 @@ api.interceptors.request.use(
   }
 );
 
+// Function to refresh the access token
+const refreshAccessToken = async () => {
+  const response = await axios.post(`${PUBLIC_API_URL}/usuarios/refrescar-token`, {
+    refresh_token: refreshToken, // Assuming the refresh token is stored
+  });
+
+  const newAccessToken = response.data.access_token;
+
+  currentAuthTokens.set({
+    access_token: newAccessToken,
+    refresh_token: refreshToken!,
+  });
+
+  return newAccessToken;
+};
+
+
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    // TODO: This error code should be changed whenever token expires are
+    // handled different from invalid tokens
+    if (error.response?.status === 403 && accessToken && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const newAccessToken = await refreshAccessToken();
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return api(originalRequest);
+      } catch (refreshError) {
+        currentAuthTokens.set(null);
+        return Promise.reject(refreshError);
+      }
+    }
+
+    // If the error is not a 403 or there's no token, reject the error
+    return Promise.reject(error);
+  }
+);
+
+
 export default api;
